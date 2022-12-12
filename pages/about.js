f9a4374562cf2e891878315bd2b3d89fd49b438e
KarthikeyanRamesh <karthisujatha10@gmail.com>
2022-12-12 19:55:33 +0530

    tried some different routes

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Minim sint enim adipisicing qui nisi sint sunt voluptate culpa quis amet ad. Dolor cupidatat enim aute ex ea et ullamco labore. Aliquip ea elit nulla in est do dolor esse ullamco ea adipisicing. Occaecat proident et enim minim aliqua sit amet laborum dolor tempor nulla exercitation. In culpa voluptate laborum duis reprehenderit ad.</p>
+            <p>Minim sint enim adipisicing qui nisi sint sunt voluptate culpa quis amet ad. Dolor cupidatat enim aute ex ea et ullamco labore. Aliquip ea elit nulla in est do dolor esse ullamco ea adipisicing. Occaecat proident et enim minim aliqua sit amet laborum dolor tempor nulla exercitation. In culpa voluptate laborum duis reprehenderit ad.</p>
+            <p>Minim sint enim adipisicing qui nisi sint sunt voluptate culpa quis amet ad. Dolor cupidatat enim aute ex ea et ullamco labore. Aliquip ea elit nulla in est do dolor esse ullamco ea adipisicing. Occaecat proident et enim minim aliqua sit amet laborum dolor tempor nulla exercitation. In culpa voluptate laborum duis reprehenderit ad.</p>
+            <p>Minim sint enim adipisicing qui nisi sint sunt voluptate culpa quis amet ad. Dolor cupidatat enim aute ex ea et ullamco labore. Aliquip ea elit nulla in est do dolor esse ullamco ea adipisicing. Occaecat proident et enim minim aliqua sit amet laborum dolor tempor nulla exercitation. In culpa voluptate laborum duis reprehenderit ad.</p>
+
+        </div>
+    );
+}
+ 
+export default About;
