aaae1f4946d7961f3fe27190648a10480e846684
KarthikeyanRamesh <karthisujatha10@gmail.com>
2022-12-13 18:13:26 +0530

    added some styles and added 404 component

--- pages/index.js
+++ pages/index.js
@@ -8,10 +8,10 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <h1>Homepage</h1>
-      <p>Eiusmod anim consequat aute sit id excepteur deserunt. Quis ut elit quis nostrud sunt laborum sit. Fugiat sunt ea nisi magna ea ex ex qui enim duis eiusmod amet officia. Irure laboris laborum veniam proident deserunt exercitation occaecat ipsum qui ipsum nisi esse magna. Amet commodo cillum eiusmod occaecat est pariatur duis est Lorem.</p>
-      <p>Velit cillum do exercitation et enim reprehenderit ipsum qui. Proident commodo dolore et deserunt minim enim in eiusmod laborum dolor labore nulla est. Nisi sit tempor elit aliqua aute dolore consectetur deserunt elit do mollit aliquip. Qui nostrud proident exercitation occaecat.</p>
-      <Link href="/ninjas">See Ninja Listing</Link>
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text}>Eiusmod anim consequat aute sit id excepteur deserunt. Quis ut elit quis nostrud sunt laborum sit. Fugiat sunt ea nisi magna ea ex ex qui enim duis eiusmod amet officia. Irure laboris laborum veniam proident deserunt exercitation occaecat ipsum qui ipsum nisi esse magna. Amet commodo cillum eiusmod occaecat est pariatur duis est Lorem.</p>
+      <p className={styles.text}>Velit cillum do exercitation et enim reprehenderit ipsum qui. Proident commodo dolore et deserunt minim enim in eiusmod laborum dolor labore nulla est. Nisi sit tempor elit aliqua aute dolore consectetur deserunt elit do mollit aliquip. Qui nostrud proident exercitation occaecat.</p>
+      <Link href="/ninjas" className={styles.btn}>See Ninja Listing</Link>
     </div>
   )
 }
